5a27e1b643721ff693b7fb63ae96d9746b6353b3
asiFREAK <ra0182108@gmail.com>
2023-11-06 12:56:54 +0530

    face detection

--- user_app/views.py
+++ user_app/views.py
@@ -36,6 +36,9 @@ def complete(request):
 
 @gzip.gzip_page
 def stream_camera(request):
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    face_detected = False
+
     try:
         cap = cv2.VideoCapture(0)
 
@@ -43,10 +46,21 @@ def stream_camera(request):
             raise RuntimeError("Could not start camera.")
 
         def generate():
+            nonlocal face_detected
             while True:
                 ret, frame = cap.read()
                 if not ret:
                     break
+
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+                if len(faces) > 0 and not face_detected:
+                    face_detected = True
+                elif len(faces) == 0 and face_detected:
+                    print("Face gone out of the camera")
+                    face_detected = False
+
                 _, buffer = cv2.imencode('.jpg', frame)
                 frame = buffer.tobytes()
                 yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
